736b51bdf3d3b36db869a4030dc251e2f375aef7
Esteb4ncd <davidecruzd10@gmail.com>
2026-01-27 21:42:48 -0800

    Add landing page before game starts

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
+'use client';
+
+import { useState } from 'react';
+import LandingPage from './components/LandingPage';
 import Game from './components/Game';
 
 export default function Home() {
+  const [showGame, setShowGame] = useState(false);
+
+  if (!showGame) {
+    return <LandingPage onPlay={() => setShowGame(true)} />;
+  }
+
   return <Game />;
 }
