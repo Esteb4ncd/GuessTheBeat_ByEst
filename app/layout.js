73e64179498b03702758367243af57322d632a45
Esteb4ncd <davidecruzd10@gmail.com>
2026-01-27 23:14:26 -0800

    feat: barrio font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Londrina_Sketch } from "next/font/google";
+import { Geist, Geist_Mono, Londrina_Sketch, Barrio } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -17,6 +17,12 @@ const londrinaSketch = Londrina_Sketch({
   subsets: ["latin"],
 });
 
+const barrio = Barrio({
+  weight: "400",
+  variable: "--font-barrio",
+  subsets: ["latin"],
+});
+
 export const metadata = {
   title: "Guess The Beat by EST",
   description: "Test your music knowledge by guessing songs from 5-second previews!",
@@ -25,7 +31,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${londrinaSketch.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${londrinaSketch.variable} ${barrio.variable}`}>
         {children}
       </body>
     </html>
